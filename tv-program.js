cee22f4586f60990367a57a29ae11a397760466b
Hiramatsu Kakeru <258475@st.takushoku-u.ac.jp>
2026-05-28 17:31:16 +0900

    tvを編集

--- tv-program.js
+++ tv-program.js
@@ -1,27 +1,62 @@
 
 // 課題3-2 のプログラムはこの関数の中に記述すること
 function print(data) {
-  console.log(data.list.g1.service);
+  console.log("1件目の検索結果:"); 
+  for(let n of data.list.g1){
+  console.log(n.service.name);
+  console.log("番組名:" + n.title);
+  console.log( n.subtitle);
+  console.log("概要"+ n.content);
+  }
 }
 
-// 課題5-1 の関数 printDom() はここに記述すること
-function printDom(data) {
+// 課題5-1 の関数 printDom() はここに記述するこ
 
+function printDom(data) {
+  let dr =document.createElement('div');
+  dr.setAttribute('div','result');
+  let u=document.createElement('ul');
+  dr.insertAdjacentElement('beforeend',u);
+  for(let n of data.list.g1){
+    let l=document.createElement('li');
+    l.textContent=n.service.name;
+    u.insertAdjacentElement('beforeend',l);
+    l=document.createElement('li');
+    l.textContent="番組名:" + n.title;
+    u.insertAdjacentElement('beforeend',l);
+    l=document.createElement('li');
+    l.textContent=n.subtitle;
+    u.insertAdjacentElement('beforeend',l);
+    l=document.createElement('li');
+    l.textContent="概要:"+ n.content;
+    u.insertAdjacentElement('beforeend',l);
+  }
+  let b=document.body;
+  b.insertAdjacentElement('beforeend',dr);
 }
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
-
-
+let sr=document.querySelector('button#answer');
+sr.addEventListener('click',sendRequest);
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
-
+  let url='https://www.nishita-lab.org/web-contents/jsons/nhk/{service}-{genre}-j.json';
+  axios.get(url)
+  .then(showResult)
+  .catch(showError)
+  .then(finish);
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
-
+  let data =resp.data;
+  if(typeof data==='String'){
+    data=JSON.parse(data);
+    console.log(data);
+    console.log(data.x);
+  }
 }
 
 // 課題6-1: 通信エラーが発生した時の処理
@@ -38,84 +73,3 @@ function finish() {
 // 以下はテレビ番組表のデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
 // 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "list": {
-    "g1": [
-      {
-        "id": "2022030428673",
-        "event_id": "28673",
-        "start_time": "2022-03-04T04:35:00+09:00",
-        "end_time": "2022-03-04T04:40:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "みんなのうた「ごっつぉさま」／「超変身！ミネラルフォーマーズ」",
-        "subtitle": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "content": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "act": "",
-        "genres": [
-          "0409",
-          "0700",
-          "0504"
-        ]
-      },
-      {
-        "id": "2022030427069",
-        "event_id": "27069",
-        "start_time": "2022-03-04T23:05:00+09:00",
-        "end_time": "2022-03-04T23:10:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "パラスポーツ×アニメ「アニ×パラ」▽パラアルペンスキーテーマ曲江口寿史×ＡＣＣ",
-        "subtitle": "パラスポーツの魅力をアニメで伝える番組。高速滑走に挑む精神力が試されるパラアルペンスキーを描く。キャラ原案：江口寿史／曲：Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ",
-        "content": "パラスポーツの魅力をアニメで伝えるプロジェクトの第１３弾。圧倒的なスピードに挑む「パラアルペンスキー」の世界を江口寿史原案の魅力的なキャラクターで描く。平昌パラリンピック金メダリストの村岡桃佳選手への取材から生まれた主人公・桃は、スピードへの恐怖を克服していく。その壁を越えた先にあるものとは…　テーマ曲　♪「Ｏｎ　Ｙｏｕｒ　Ｍａｒｋ」はＡｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂが手掛けた。",
-        "act": "【声】松本まりか，【出演】Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ，【監督】西村一彦，【脚本】加納新太，【原案】江口寿史",
-        "genres": [
-          "0700"
-        ]
-      }
-    ]
-  }
-};
-
